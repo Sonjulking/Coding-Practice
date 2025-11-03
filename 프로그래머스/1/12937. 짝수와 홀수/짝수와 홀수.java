e5de40f0dfb5ffb64b97004664d21fdec46a497e
Sonjulking <113520712+Sonjulking@users.noreply.github.com>
2025-11-04 08:44:48 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int num) {
+        String res;
+        if(num % 2 == 0){
+            res ="Even";
+        
+        }else{
+            res ="Odd";
+         
+        }
+           return res;
+   
+    }
+}
