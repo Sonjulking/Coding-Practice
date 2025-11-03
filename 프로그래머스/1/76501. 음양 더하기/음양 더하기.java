3084c9f2f778036cb005c3a15c269e16a3877bca
Sonjulking <113520712+Sonjulking@users.noreply.github.com>
2025-11-03 16:05:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i < absolutes.length; i ++){
+            if(signs[i] == false){
+                answer = answer + (absolutes[i] - 2 * absolutes[i]);
+            }else{
+                answer = answer + absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
