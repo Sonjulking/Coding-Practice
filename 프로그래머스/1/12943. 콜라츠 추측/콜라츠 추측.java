11e8b954da98bd8ad37c0cb44aa90efde474e3eb
Sonjulking <113520712+Sonjulking@users.noreply.github.com>
2025-11-11 17:23:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int num) {
+        long n = (long) num; 
+        int answer = 0;
+        
+        while (n != 1 && answer < 500){ 
+            if(n % 2 == 0){
+                n /= 2;
+            }else{
+                n = n * 3 + 1; 
+            }
+            answer++;
+        }
+        
+        if(answer >= 500 && n != 1){ 
+            return -1;
+        }
+
+        return answer;
+    }
+}
