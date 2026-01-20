c9263ec743a52509e763adcd72247e102a426bee
Sonjulking <113520712+Sonjulking@users.noreply.github.com>
2026-01-20 15:45:40 +0900

    [level 2] Title: 피보나치 수, Time: 2.42 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int prev2 = 0;
+        int prev1 = 1;
+        int current = 0;
+        for(int i = 2; i <= n; i++) {
+            current = (prev1 + prev2) % 1234567;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        answer = current;
+        
+        return answer;
+    }
+}
