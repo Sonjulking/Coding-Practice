c012eaf223a6b6ad6fa211006bb9174228a85ea8
Sonjulking <113520712+Sonjulking@users.noreply.github.com>
2026-01-21 17:26:26 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.38 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<Character, Integer> lastSeen = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(lastSeen.containsKey(ch)){
+                answer[i] = i - lastSeen.get(ch);
+            }else{
+                answer[i] = -1;
+            }
+            
+            lastSeen.put(ch, i);
+            
+        }
+        
+        return answer;
+        
+    }
+}
